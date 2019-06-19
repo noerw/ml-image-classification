42992055a62343478f9661c587de5ed191316dbd
Benni Dietz <b_diet05@uni-muenster.de>
2019-06-19 16:15:17 +0200

    Refactor number of predictors

--- project/apply_mlp.py
+++ project/apply_mlp.py
@@ -29,10 +29,12 @@ others_data = getAllPixels(other_data, 3)
 whole_image_data = getAllPixels(whole_image, 4)
 all_training_data = np.array(sky_data + river_data + others_data)
 
-net = mlp.mlp(all_training_data[:,0:3], all_training_data[:,3:],1)
-net.mlptrain(all_training_data[:,0:3], all_training_data[:,3:], 0.25, 100)
+number_predictors = len(all_training_data[0]) - 1
+
+net = mlp.mlp(all_training_data[:,0:number_predictors], all_training_data[:,number_predictors:],1)
+net.mlptrain(all_training_data[:,0:number_predictors], all_training_data[:,number_predictors:], 0.25, 100)
 print(net)
-net.confmat(all_training_data[:,0:3], all_training_data[:,3:])
+net.confmat(all_training_data[:,0:number_predictors], all_training_data[:,number_predictors:])
 prediction = net.mlpfwd(whole_image_data)
 print(len(prediction))
 print(prediction[0])
@@ -43,11 +45,11 @@ classified_pixels = img.load()
 for x in range(whole_width):
     for y in range(whole_height):
         if int(prediction[i][0]) == 1:
-            classified_pixels[x, y] = (0,0,0)
+            classified_pixels[x, y] = (255,0,0)
         elif int(prediction[i][0]) == 2:
-            classified_pixels[x, y] = (127, 127, 127)
+            classified_pixels[x, y] = (0, 255, 0)
         else:
-            classified_pixels[x, y] = (255, 255, 255)
+            classified_pixels[x, y] = (0, 0, 255)
         i = i + 1
 img.show()       
 img.save("classificated_pic.jpg")
